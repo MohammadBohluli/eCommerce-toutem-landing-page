2da7a426fad94ffaf3c0f7bfb2dcac88a9f07fca
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-01-22 11:56:41 +0330

    create search button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const collapsible = document.querySelector(".collapsible");
 const navList = document.querySelector(".nav__list");
 const searchBox = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".search-btn");
 
 const handlerClick = function () {
   navList.classList.toggle("show-content");
   searchBox.classList.toggle("show-content");
 };
 collapsible.addEventListener("click", handlerClick);
+
+searchBtn.addEventListener("click", () => {
+  searchBox.classList.toggle("active");
+});
