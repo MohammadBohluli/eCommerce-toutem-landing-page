2535b1b01c150353cce47a5bb58bd947a34892e6
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-01-22 15:27:25 +0330

    bugfix: change position navbar

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ const collapsible = document.querySelector(".collapsible");
 const navList = document.querySelector(".nav__list");
 const searchBox = document.querySelector(".search-box");
 const searchBtn = document.querySelector(".search-btn");
+const searchWrapper = document.querySelector(".search-wrapper");
 
 const handlerClick = function () {
   navList.classList.toggle("show-content");
   searchBox.classList.toggle("show-content");
+  searchWrapper.classList.toggle("show-content");
 };
 collapsible.addEventListener("click", handlerClick);
 
